96477b38d99fbb8d0033e056162e8848e4da0a7e
Michael Banzeu <banzeumike@gmail.com>
2023-08-02 18:11:42 +0100

    JS script updated & minor changes of the CSS

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function color(params) {
-    var a = getComputedStyle(params);
-    var b = a.backgroundColor;
-    document.getElementsByTagName("body")[0].style.backgroundColor = b;
+let colors = document.getElementsByTagName('span');
+for (const color of colors) {
+    color.addEventListener('click', switchColor);
+}
+
+function switchColor(e) {
+    document.getElementsByTagName('body')[0].style.backgroundColor = getComputedStyle(e.target).backgroundColor;
 }
